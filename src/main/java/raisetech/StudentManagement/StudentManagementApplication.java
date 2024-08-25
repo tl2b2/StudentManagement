3e1b5daafd0aaadf13eedb8bd295149227adca42
tl2b2 <tl2b2hyllos@gmail.com>
2024-08-25 15:50:41 +0900

    テストです

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,44 @@
 package raisetech.StudentManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	@Autowired
+	private StudentRepository repository;
+
+	private final String name = "Enami Kouji";
+	private final String age ="37";
+	private final String job ="teacher";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello,World";
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name){
+		Student student = repository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
+
+    }
+
+	@PostMapping("/student")
+	public void registerInfo(String name, int age){
+		repository.registerStudent(name,age);
 	}
+
+	@PatchMapping("/student")
+	public void updateStudentName(String name, int age){
+		repository.updateStudent(name,age);
+    }
+	@DeleteMapping ("/student")
+	public void deleteStudent(String name){
+		repository.deleteStudent(name);
+	}
+
 }
