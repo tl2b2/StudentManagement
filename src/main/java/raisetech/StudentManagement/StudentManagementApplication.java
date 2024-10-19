c4f896a63c0ad3b7c1b62c12c05444da16dea69f
tl2b2 <tl2b2hyllos@gmail.com>
2024-10-19 16:37:35 +0900

    Merge kadaino10
    # Conflicts:
    #	src/main/java/raisetech/StudentManagement/StudentRepository.java

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,44 +1,56 @@
 package raisetech.StudentManagement;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.Student;
+import raisetech.StudentManagement.StudentRepository;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	@Autowired
-	private StudentRepository repository;
+    @Autowired
+    private StudentRepository repository;
 
-	private final String name = "Enami Kouji";
-	private final String age ="37";
-	private final String job ="teacher";
+    private String name ="Fujitaka Jubiro";
+    private String age = "60";
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
-
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name){
-		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "歳";
-
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementApplication.class, args);
     }
 
-	@PostMapping("/student")
-	public void registerInfo(String name, int age){
-		repository.registerStudent(name,age);
-	}
-
-	@PatchMapping("/student")
-	public void updateStudentName(String name, int age){
-		repository.updateStudent(name,age);
+    @GetMapping("/student")
+    public String getStudentInfo() {
+        Student student = repository.searchByName("TakahasiRyouji");
+        return student.getName() + " " + student.getAge() + "歳";
     }
-	@DeleteMapping ("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-	}
 
+    @PostMapping("/student")
+    public void registerStudent(String name,String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @PatchMapping("/student")
+    public void updateStudent(String name, int age) {
+        repository.updateStudent(name, age);
+    }
+
+    @DeleteMapping("/student")
+    public void deleteStudent(String name) {
+        repository.deleteStudent(name);
+    }
 }
